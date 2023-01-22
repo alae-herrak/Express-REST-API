23c92a35cc2ff42dda6f303a69b30c19be9a95ce
alaeh-errak <alaeherrak@gmail.com>
2023-01-22 15:47:52 +0100

    created the User Schema

--- models/User.js
+++ models/User.js
@@ -1,1 +1,18 @@
 const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Users", UserSchema);
