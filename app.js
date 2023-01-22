19cd53ef8bc7edaa3c763d19d1f88863eb22b442
alaeh-errak <alaeherrak@gmail.com>
2023-01-22 16:38:29 +0100

    created GET and POST logic

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const usersRoute = require("./routes/users");
 require("dotenv").config();
 
-mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGO_DB_URL, () =>
-  console.log("Connected to DB")
-);
+app.use(bodyParser.json());
+app.use("/users", usersRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server started and running on port ${PORT}`);
-});
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then(() => {
+    console.log("Connected to DB");
+    app.listen(PORT, () => {
+      console.log(`Server started and running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`connexion to DB failed: ${err}`);
+  });
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,28 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("users");
+router.post("/", async (req, res) => {
+  const user = new User({
+    fullName: req.body.fullName,
+    email: req.body.email,
+    phoneNumber: req.body.phoneNumber,
+  });
+  try {
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
 module.exports = router;
